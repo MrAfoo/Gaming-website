b424710518831d4601376078188c0149508880b2
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-01-14 18:08:59 +0500

    Gaming Website

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,24 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        inlineDynamicImports: true
+      }
+    },
+    sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  },
+  server: {
+    host: true,
+    strictPort: true
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
-
-
